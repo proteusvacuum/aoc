e68f7c4741fc5acb6eb787335eaf9576f6ae54dd
Farid Rener <proteusvacuum@gmail.com>
2024-12-11 21:12:45 -0500

    2024/11-2 attempt 2
    Oh, duh.

--- 2024/11/11-2.py
+++ 2024/11/11-2.py
@@ -9,28 +9,26 @@ stones = [int(n) for n in line.split(" ")]
 stone_counts = defaultdict(int)
 for stone in stones:
     stone_counts[stone] += 1
-print(stone_counts)
 
 for _ in range(75):
-    print(_)
     new_stone_counts = stone_counts.copy()
-
     for stone_value, stone_count in stone_counts.items():
-        for i in range(stone_count):
-            if stone_value == 0:
-                new_stone_counts[0] -= 1
-                new_stone_counts[1] += 1
-            elif len(str(stone_value)) % 2 == 0:
-                new_stone_counts[stone_value] -= 1
-                str_num = str(stone_value)
-                first_half = int(str_num[:len(str_num) // 2])
-                second_half = int(str_num[len(str_num) // 2:])
-                new_stone_counts[first_half] += 1
-                new_stone_counts[second_half] += 1
-            else:
-                new_stone_counts[stone_value] -= 1
-                new_stone_counts[stone_value * 2024] += 1
+        if stone_count == 0:
+            del new_stone_counts[stone_value]
+            continue
+        if stone_value == 0:
+            new_stone_counts[0] -= stone_count
+            new_stone_counts[1] += stone_count
+        elif len(str(stone_value)) % 2 == 0:
+            new_stone_counts[stone_value] -= stone_count
+            str_num = str(stone_value)
+            first_half = int(str_num[: len(str_num) // 2])
+            second_half = int(str_num[len(str_num) // 2 :])
+            new_stone_counts[first_half] += stone_count
+            new_stone_counts[second_half] += stone_count
+        else:
+            new_stone_counts[stone_value] -= stone_count
+            new_stone_counts[stone_value * 2024] += stone_count
 
-    stone_counts = new_stone_counts.copy()
-
+    stone_counts = new_stone_counts
 print(sum(stone_counts.values()))
