51a2d0170176a33b9ca68e05b52fa99064137741
Farid Rener <proteusvacuum@gmail.com>
2025-12-07 15:31:06 -0500

    Make grid generic

--- 2025/day_7/src/main.rs
+++ 2025/day_7/src/main.rs
@@ -7,13 +7,13 @@ struct Coord {
 }
 
 #[derive(Debug, Clone)]
-struct Grid {
-    height: usize,    // y
-    width: usize,     // x
-    cells: Vec<char>, // Use a flat vec, we can calculate the indices based on the height / width
+struct Grid<T> {
+    height: usize, // y
+    width: usize,  // x
+    cells: Vec<T>, // Use a flat vec, we can calculate the indices based on the height / width
 }
-impl Grid {
-    fn new(height: usize, width: usize, cells: Vec<char>) -> Self {
+impl<T> Grid<T> {
+    fn new(height: usize, width: usize, cells: Vec<T>) -> Self {
         Self {
             height,
             width,
@@ -25,16 +25,19 @@ impl Grid {
         coord.y * self.width + coord.x
     }
 
-    fn get(&self, coord: Coord) -> Option<char> {
+    fn get(&self, coord: Coord) -> Option<T>
+    where
+        T: Copy,
+    {
         self.cells.get(self.cell_id(coord)).copied()
     }
 
-    fn set(&mut self, coord: Coord, value: char) {
+    fn set(&mut self, coord: Coord, value: T) {
         let cell_id = self.cell_id(coord);
         self.cells[cell_id] = value;
     }
 
-    fn set_left(&mut self, coord: Coord, value: char) {
+    fn set_left(&mut self, coord: Coord, value: T) {
         let nx = coord.x as isize - 1;
         let ny = coord.y;
         if nx < 0 {
@@ -48,7 +51,7 @@ impl Grid {
         self.cells[cell_id] = value;
     }
 
-    fn set_right(&mut self, coord: Coord, value: char) {
+    fn set_right(&mut self, coord: Coord, value: T) {
         let nx = coord.x + 1;
         let ny = coord.y;
         if nx > self.width {
@@ -63,7 +66,10 @@ impl Grid {
         coord.x < self.width && coord.y < self.height
     }
 
-    fn iter_values(&self) -> impl Iterator<Item = (Coord, Option<char>)> {
+    fn iter_values(&self) -> impl Iterator<Item = (Coord, Option<T>)>
+    where
+        T: Copy,
+    {
         self.cells.iter().enumerate().map(|(i, v)| {
             let x = i % self.width;
             let y = i / self.width;
@@ -71,7 +77,10 @@ impl Grid {
         })
     }
 
-    fn above(&self, coord: &Coord) -> Option<(Coord, char)> {
+    fn above(&self, coord: &Coord) -> Option<(Coord, T)>
+    where
+        T: Copy,
+    {
         // What is above this cell?
         let nx = coord.x;
         let ny = coord.y as isize - 1;
@@ -93,45 +102,9 @@ impl Grid {
             None
         }
     }
-
-    fn neighbours(&self, coord: &Coord) -> impl Iterator<Item = (Coord, char)> {
-        const DIRECTIONS: [(isize, isize); 8] = [
-            (-1, -1),
-            (0, -1),
-            (1, -1),
-            (-1, 0),
-            (1, 0),
-            (-1, 1),
-            (0, 1),
-            (1, 1),
-        ];
-
-        DIRECTIONS.into_iter().filter_map(move |(dx, dy)| {
-            let nx = coord.x as isize + dx;
-            let ny = coord.y as isize + dy;
-
-            if nx < 0 || ny < 0 {
-                return None;
-            }
-
-            let new_coord: Coord = Coord {
-                x: nx as usize,
-                y: ny as usize,
-            };
-
-            if self.in_bounds(new_coord) {
-                Some((
-                    new_coord,
-                    self.get(new_coord).expect("out of bounds somehow"),
-                ))
-            } else {
-                None
-            }
-        })
-    }
 }
 
-impl Display for Grid {
+impl<T: std::fmt::Display> Display for Grid<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for y in 0..self.height {
             for x in 0..self.width {
@@ -146,7 +119,7 @@ impl Display for Grid {
     }
 }
 
-fn parse_input(input: &str) -> Grid {
+fn parse_input(input: &str) -> Grid<char> {
     let width = input.lines().next().unwrap().len();
     let cells: Vec<char> = input.lines().flat_map(str::chars).collect();
     Grid::new(input.lines().count(), width, cells)
@@ -196,36 +169,35 @@ fn solve_part2_again(input: &str) -> usize {
     // NOTE: I chatted with Claude about this approach, but coded this up myself.
     // Store the number of particles flowing down each branch, then count it at the end
 
-    // let mut count = 1;
-    // let grid = parse_input(input);
-    // let mut new_grid = grid.clone();
+    let mut count = 1;
+    let grid = parse_input(input);
+    let mut new_grid = grid.clone();
 
-    // grid.iter_values().for_each(|(coord, c)| {
-    //     let current_char = c.unwrap();
-    //     if let Some((_, above)) = new_grid.above(&coord) {
-    //         match current_char {
-    //             '.' => match above {
-    //                 'S' | '|' => {
-    //                     new_grid.set(coord, '|');
-    //                 }
-    //                 _ => {}
-    //             },
-    //             '^' => match above {
-    //                 'S' | '|' => {
-    //                     // On either side we set it as a '|'
-    //                     new_grid.set_left(coord, '|');
-    //                     new_grid.set_right(coord, '|');
-    //                     count *= 2;
-    //                 }
-    //                 _ => {}
-    //             },
-    //             _ => {}
-    //         };
-    //     }
-    // });
+    grid.iter_values().for_each(|(coord, c)| {
+        let current_char = c.unwrap();
+        if let Some((_, above)) = new_grid.above(&coord) {
+            match current_char {
+                '.' => match above {
+                    'S' | '|' => {
+                        new_grid.set(coord, '|');
+                    }
+                    _ => {}
+                },
+                '^' => match above {
+                    'S' | '|' => {
+                        // On either side we set it as a '|'
+                        new_grid.set_left(coord, '|');
+                        new_grid.set_right(coord, '|');
+                        count *= 2;
+                    }
+                    _ => {}
+                },
+                _ => {}
+            };
+        }
+    });
 
-    // count
-    todo!();
+    count
 }
 
 fn solve_part2(input: &str) -> usize {
@@ -234,7 +206,7 @@ fn solve_part2(input: &str) -> usize {
 
     let grid = parse_input(input);
 
-    fn split_world(grid: &Grid, starting_idx: usize, mut count: usize) -> usize {
+    fn split_world(grid: &Grid<char>, starting_idx: usize, mut count: usize) -> usize {
         let mut new_grid = grid.clone();
         grid.iter_values()
             .enumerate()
@@ -266,8 +238,6 @@ fn solve_part2(input: &str) -> usize {
                     };
                 }
             });
-        println!("{}", new_grid);
-        println!("---");
         count
     }
 
